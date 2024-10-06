059f8f914267138e5f5b8da2774a0d025c18c57c
Vladimir Elisavetski <yvova88@yahoo.com>
2024-10-06 14:39:53 -0600

    getting event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest/db"
 	"example.com/rest/models"
@@ -12,6 +13,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
+	server.GET("/events/:id", getEvent)
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
 	server.Run(":8080")
@@ -24,6 +26,24 @@ func getEvents(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldnt fetch events"})
 	}
 	context.JSON(200, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Coulndt parse event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Coulndt fetch event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
