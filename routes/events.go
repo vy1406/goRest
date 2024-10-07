21e5fb64b04a8f006bae9baa91f2f2af08cf1b13
Vladimir Elisavetski <yvova88@yahoo.com>
2024-10-06 19:28:01 -0600

    deleting event

--- routes/events.go
+++ routes/events.go
@@ -89,3 +89,28 @@ func updateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated", "event": updatedEvent})
 }
+
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Coulndt parse event id"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Coulndt fetch event"})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Couldnt delete event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted"})
+}
